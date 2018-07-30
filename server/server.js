3f03faff06aa1350b1cab15d28107ab6497f2d7d
Gregory Hart <gskhrvat@yahoo.com>
2018-07-30 13:22:41 -0600

    DB running

--- server/server.js
+++ server/server.js
@@ -5,13 +5,17 @@ const express = require("express"),
 
 app = express();
 
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 // MIDDLEWARE
 app.use(bodyParser.json());
 
 // ENDPOINTS
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Blimey, matey, heave ho on port: ${SERVER_PORT}`);
+massive(CONNECTION_STRING).then(db => {
+    app.set("db", db);
+    
+    app.listen(SERVER_PORT, () => {
+        console.log(`Blimey, matey, heave ho on port: ${SERVER_PORT}`);
+    })
 })
