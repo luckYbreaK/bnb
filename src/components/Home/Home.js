24e997fb9775a99981e2cb82e136f971be122886
Gregory Hart <gskhrvat@yahoo.com>
2018-07-31 14:35:01 -0600

    Created Home.js skeleton

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,37 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import axios from "axios";
-import {updateSuites} from "../../ducks/reducer";
+import { updateSuites } from "../../ducks/reducer";
+import "./Home.css";
 
 class Home extends Component {
-    // componentDidMount() {
-    //     axios.get("/api/suites").then(res => {
-    //         console.log(res.data);
-            
-    //     });
-    // }
+    componentDidMount() {
+        let { updateSuites } = this.props;
+        axios.get("/api/suites").then(res => {
+            updateSuites(res.data);
+        });
+    }
 
     render() {
+        let { suites } = this.props
+        let random = suites ? Math.floor(Math.random() * suites.length) : 0;
         return (
-            <div>Home</div>
+            <div className="home_container">
+                <div className="card_container">
+                    Home
+                {/* displays a random suite */}
+                    {suites[random] ? suites[random].title : ""}
+                </div>
+                <div className="specials_container">
+                    Specials
+                </div>
+                <div className="card_container">
+                    Castle Creek Img
+                </div>
+                <div>
+                    Castle Creek Info
+                </div>
+            </div>
         );
     }
 }
@@ -24,4 +42,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, {updateSuites})(Home);
+export default connect(mapStateToProps, { updateSuites })(Home);
