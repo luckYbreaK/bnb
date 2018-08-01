f725b983bfa209dd708a30d2cfed0289c4a7ab10
Gregory Hart <gskhrvat@yahoo.com>
2018-08-01 14:47:07 -0600

    Implemented dropdown menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,57 @@
-import React from "react";
-import "./Nav.css";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import logo from "../../img/logo/logo.png";
 
-export default function Nav() {
-    return (
-        <div className="navbar">
-            <div className="logo">
-                <img src={logo} alt="" />
-                <h2>801-000-0000</h2>
+import "./Nav.css";
+
+export default class Nav extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            showMenu: false
+        }
+
+        
+    }
+
+    // showMenu() {
+    //     this.setState({
+    //         showMenu: 
+    //     });
+    // }
+
+    render() {
+        return (
+            <div className="navbar">
+                <div className="logo">
+                    <Link to="/"><img src={logo} alt="" /></Link>
+                    <h2>801-000-0000</h2>
+                </div>
+                <div className="menu">
+                    <img
+                        src="https://cdn4.iconfinder.com/data/icons/flat-black/512/menu.png"
+                        alt="hamburger menu"
+                        onClick={() => this.setState({ showMenu: !this.state.showMenu })}
+                    />
+                    {
+                        this.state.showMenu
+                            ? (
+                                <div>
+                                    <ul>
+                                        <Link to="/suites"><li>Suites</li></Link>
+                                        <li>Register</li>
+                                        <li>Sign In</li>
+                                    </ul>
+                                </div>
+                            )
+                            : (
+                                null
+                            )
+                    }
+                </div>
             </div>
-            <div className="menu">
-                <img src="https://cdn4.iconfinder.com/data/icons/flat-black/512/menu.png" alt="" />
-            </div>
-        </div>
-    );
+        );
+    }
 }
